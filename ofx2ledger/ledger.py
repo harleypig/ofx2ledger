b5549158054bd40321f60facf931f7839b4accae
Alan Young <harleypig@harleypig.com>
2023-09-24 14:35:30 -0600

    Refactored the Ledger class to improve readability and consistency in the code.

--- ofx2ledger/ledger.py
+++ ofx2ledger/ledger.py
@@ -1,13 +1,14 @@
 import os
 
-# ----------------------------------------------------------------------------
+##############################################################################
 class Ledger:
     """
-    A class to handle ledger files.
+    A class to get information about ledger files.
 
-    This class takes an optional filename as an argument and sets it to self.filename.
-    If no filename is passed, it checks for $HOME/.hledger.journal and sets filename to that,
-    otherwise it sets filename to None.
+    This class takes an optional filename as an argument and sets it to
+    self.filename.  If no filename is passed, it checks for
+    $HOME/.hledger.journal and sets filename to that, otherwise it sets
+    filename to None.
 
     Attributes:
         filename (str): The path to the ledger file.
@@ -17,8 +18,9 @@ class Ledger:
         """
         Initialize the Ledger object.
 
-        This method sets the filename attribute. If no filename is passed, it checks for
-        $HOME/.hledger.journal and sets filename to that, otherwise it sets filename to None.
+        This method sets the filename attribute. If no filename is passed, it
+        checks for $HOME/.hledger.journal and sets filename to that, otherwise
+        it sets filename to None.
 
         Args:
             filename (str, optional): The path to the ledger file. Defaults to None.
