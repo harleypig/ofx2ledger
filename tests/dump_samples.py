d3e40f25eabddc513d10df64b00658950b7adf9c
Alan Young <harleypig@harleypig.com>
2024-03-22 09:11:24 -0600

    Refactored rendering of transaction samples to use LiteralScalarString for expected output.

--- tests/dump_samples.py
+++ tests/dump_samples.py
@@ -1,6 +1,8 @@
 import yaml
 import os
+
 from jinja2 import Environment, FileSystemLoader, select_autoescape
+from yaml.scalarstring import LiteralScalarString
 
 env = Environment(
     loader=FileSystemLoader(os.path.join(os.path.dirname(__file__), '..')),
@@ -8,10 +10,10 @@ env = Environment(
     lstrip_blocks=False,
     trim_blocks=False
 )
-                  autoescape=select_autoescape(['html', 'xml', 'j2']))
+
+template = env.get_template('transaction.j2')
 
 def render_transaction(transaction_data):
-    template = env.get_template('transaction.j2')
     rendered = template.render(transaction=transaction_data, indent='  ').strip()
     return rendered
 
@@ -19,7 +21,7 @@ with open('transaction_samples.yaml', 'r') as file:
     samples = yaml.safe_load(file)
 
 for sample in samples['transaction_samples']:
-    sample['expected_output'] = render_transaction(sample['transaction_data'])
+    sample['expected_output'] = LiteralScalarString(render_transaction(sample['transaction_data']))
 
 with open('temp.yaml', 'w') as file:
     yaml.dump(samples, file, default_flow_style=False, allow_unicode=True)
