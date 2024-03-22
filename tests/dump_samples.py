c132ff33b06bbecb10780d37e35097dee0b7109d
Alan Young <harleypig@harleypig.com>
2024-03-22 09:39:39 -0600

    Refactored code for better readability and maintainability.

--- tests/dump_samples.py
+++ tests/dump_samples.py
@@ -1,29 +1,33 @@
 import yaml
 import os
-from yaml.representer import SafeRepresenter
+
+from jinja2 import Environment, FileSystemLoader, select_autoescape
+
 
 def literal_presenter(dumper, data):
     if '\n' in data:
-        return dumper.represent_scalar('tag:yaml.org,2002:str', data, style='|')
+        return dumper.represent_scalar('tag:yaml.org,2002:str',
+                                       data,
+                                       style='|')
     return dumper.represent_scalar('tag:yaml.org,2002:str', data)
+
 
 yaml.add_representer(str, literal_presenter)
 
-from jinja2 import Environment, FileSystemLoader, select_autoescape
-from yaml.scalarstring import LiteralScalarString
-
-env = Environment(
-    loader=FileSystemLoader(os.path.join(os.path.dirname(__file__), '..')),
-    autoescape=select_autoescape(['html', 'xml', 'j2']),
-    lstrip_blocks=False,
-    trim_blocks=False
-)
+env = Environment(loader=FileSystemLoader(
+    os.path.join(os.path.dirname(__file__), '..')),
+                  autoescape=select_autoescape(['html', 'xml', 'j2']),
+                  lstrip_blocks=False,
+                  trim_blocks=False)
 
 template = env.get_template('transaction.j2')
 
+
 def render_transaction(transaction_data):
-    rendered = template.render(transaction=transaction_data, indent='  ').strip()
+    rendered = template.render(transaction=transaction_data,
+                               indent='  ').strip()
     return rendered
+
 
 with open('transaction_samples.yaml', 'r') as file:
     samples = yaml.safe_load(file)
@@ -33,4 +37,3 @@ for sample in samples['transaction_samples']:
 
 with open('temp.yaml', 'w') as file:
     yaml.dump(samples, file, default_flow_style=False, allow_unicode=True)
-
